1c159bbde5620129e75d3787ec340bec6ff5f8a4
Seongho Baek <seonghobaek@gmail.com>
2018-05-28 15:56:31 +0900

    .

--- ws_tts.py
+++ ws_tts.py
@@ -9,37 +9,55 @@ from datetime import datetime
 import random
 
 import json
-from websocket import create_connection
 import redis
+
+from websocket import create_connection
+#getLogger("websocket")
 
 from user_names import name_dict
 from sentences import sentences_map
 
-#redis_conn = redis.StrictRedis(host='airi3.local',port=6379);
-redis_conn = redis.StrictRedis(host='127.0.0.1',port=6379);
-pub = redis_conn.pubsub()
-pub.subscribe('tts2')
+def ws_tts():
+    #redis_conn = redis.StrictRedis(host='airi3.local',port=6379);
 
-while True:
-    msg = pub.get_message(timeout=120)
-    if msg is None:
-        logger.debug(('input wait timeout',))
-        continue
+    redis_conn = redis.StrictRedis(host='127.0.0.1',port=6379);
+    pub = redis_conn.pubsub()
+    pub.subscribe('tts2')
 
-    logger.debug(('msg=',msg))
+    ws_url = 'ws://{:s}:{:d}/'.format('127.0.0.1',3000)
 
-    if msg['type'] == 'message':
-        if 'data' in msg:
-            user_id = msg['data']
-            try:
-                user_name = name_dict[user_id][0]
-                now = datetime.now()
-                hr  = now.hour
-                sen = random.choice(sentences_map[hr])
-                txt = user_name + ', ' + sen
-                logger.debug(('user_id=',user_id,'hr=',hr))
-                print('txt=',txt,file=sys.stderr)
-            except:
-                logger.error(('msg handling error',msg))
+    def ws_send(msg_dict, ws=None):
+        ws_ = ws if ws else create_connection(ws_url)
+        ws_.send(json.dumps(msg_dict))
+        result = ws_.recv()
+        if not ws and ws_:
+            ws_.close()
 
+    while True:
+        msg = pub.get_message(timeout=120)
+        if msg is None:
+            logger.debug(('input wait timeout',))
+            continue
+
+        logger.debug(('msg=',msg))
+
+        if msg['type'] == 'message':
+            if 'data' in msg:
+                user_id = msg['data']
+                try:
+                    user_name = name_dict[user_id][0]
+                    user_name = user_name.replace(' ','')
+                    now = datetime.now()
+                    hr  = now.hour
+                    sen = random.choice(sentences_map[hr])
+                    txt = user_name + ', ' + sen
+                    logger.debug(('user_id=',user_id,'hr=',hr))
+                    print('txt=',txt,file=sys.stderr)
+                    ws_send(dict(tts=txt))
+                except:
+                    logger.error(('msg handling error',msg))
+
+
+if __name__ == '__main__':
+    ws_tts()
 
